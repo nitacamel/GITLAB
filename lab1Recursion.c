11c3614f3d3de50b54af51598a20e42682a38da1
nitacamel <nichanita24@gmail.com>
2017-04-23 00:18:54 +0700

    Modified lab1Recursion.c

--- lab1Recursion.c
+++ lab1Recursion.c
@@ -1,26 +1,8 @@
 #include <stdio.h>
-long Factorial(int n)
-{
-    int Sum;
-     if(n == 0)
-     {
-          printf("0! is base case return answer of 0! = 1 \n");
-          return 1;
-     }
-     else
-     {
-          printf("%d! is recursive case, Answer = %d * recursion of %d!\n", n, n, n-1);
-          printf("Recursion to calculate %d!\n", n-1);
-          Sum = n*Factorial(n-1);
-          printf("Calculate %d! complete.\n", n-1);
-          printf("Return answer from %d! = %d to calculate %d! = [%d * %d!] = %d * %d = %d\n", n-1, Sum/n, n, n, n-1, n, Sum/n, Sum);
-          return Sum;
-     }
-}
 void main()
 {
     int n;
-    char answer[20];
+    char answer[50];
     char ch;
     printf("My Recursion Programs.");
     printf("\nProgram calculate n! by recursion(n<=15)");
@@ -43,4 +25,23 @@ void main()
     }while(strcmp(answer,"y")==0);
     printf("End Program.");
     printf("\nProgram writen by 57070501062 Nichanat Poonsawat");
+}
+
+long Factorial(int n)
+{
+    int Sum;
+     if(n == 0)
+     {
+          printf("0! is base case return answer of 0! = 1 \n");
+          return 1;
+     }
+     else
+     {
+          printf("%d! is recursive case, Answer = %d * recursion of %d!\n", n, n, n-1);
+          printf("Recursion to calculate %d!\n", n-1);
+          Sum = n*Factorial(n-1);
+          printf("Calculate %d! complete.\n", n-1);
+          printf("Return answer from %d! = %d to calculate %d! = [%d * %d!] = %d * %d = %d\n", n-1, Sum/n, n, n, n-1, n, Sum/n, Sum);
+          return Sum;
+     }
 }
